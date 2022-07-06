a1f44c07179125535613f2b18a062f2b509852ee
Diego Loaiza <dfloaiza@gmail.com>
2022-07-05 21:15:35 -0500

    Update PreparacionTaller1.py

--- Semana2/Practica1/PreparacionTaller1.py
+++ Semana2/Practica1/PreparacionTaller1.py
@@ -1,50 +1,72 @@
 import pandas as pd
+import datetime
 
-#cargar el data frame tal como viene:
-df_v1 = pd.read_csv("bikes_dirty.csv", 
-                    sep=';', 
-                    encoding='latin1', 
-                    infer_datetime_format=True,
-                    dayfirst=True,
-                    parse_dates=['Fecha'])
+import matplotlib.pyplot as plt
 
-#ver información previa del dataset                    
-print(df_v1.info())
 
-df_v1.dropna(inplace=True,
-             axis=1,
-             how='all' #all, any
-)
-print(df_v1.info())
+#parametros de graficacion:
+# Make the graphs a bit prettier, and bigger
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = (15, 5)
+plt.rcParams['font.family'] = 'sans-serif'
 
-#datos atipicos:
-rows = df_v1.shape[0]
-print(rows)
+# This is necessary to show lots of columns in pandas 0.12. 
+# Not necessary in pandas 0.13.
+pd.set_option('display.width', 5000) 
+pd.set_option('display.max_columns', 60)
 
-c6_st_dev = df_v1['Cra. 1'].std(axis=0)
+#Cargar dataset:
+df_bikes1 = pd.read_csv("bikes_dirty.csv",  #ruta del archivo plano
+                         encoding="latin1", #juego de caracteres del archivo
+                         sep=";",           #caracter usado como separador
+                         parse_dates=['Fecha'],
+                         dayfirst=True
+                         )   
 
-print(c6_st_dev)
-c6_mean = df_v1['Cra. 1'].mean()
+df_bikes1['Fecha'] = pd.to_datetime(df_bikes1['Fecha'], infer_datetime_format=True)
+#ver info del dataset:
+print(df_bikes1.info)
 
-#outliers y formato erróneo
-for i in range(0, rows):
-    if df_v1.iloc[i,7] >= 2*c6_st_dev:
-        df_v1.iloc[i,7] = c6_mean
-    #df_v1.iloc[i,0] = pd.to_datetime(df_v1.iloc[i,0], infer_datetime_format=True)
+#ver dataset:
+print(df_bikes1)
 
-df_v1['Fecha'] = pd.to_datetime(df_v1['Fecha'])
+#limpiar dataset
+#datos nulos
+df_bikes1.dropna(axis=1,        #"axis" indica el eje en el cual se examinan los valores nulos: 0 = por columna, 1 = por fila
+                 inplace=True,  #"inplace" indica que el cambio se aplica al propio dataframe
+                 how='all')     #"how" indica el criterio para eliminar los datos. "all", elimina si todos los datos en el eje son nulos,
+                                # "any", si encuentra al menos un valor nulo
 
-#duplicados   
-print("Duplicados:")
-print(df_v1.duplicated())
+#datos atípicos
 
-df_v1.drop_duplicates(inplace=True)
 
-df_v1 = df_v1.set_index(['Fecha'])
+# datos duplicados                                
+                
+                
+#mostrar dataset limpio:
+print(df_bikes1)
 
-df_v1_9a = df_v1[ ['Calle 9a (66-39)'] ].copy()
+print(df_bikes1.index)
 
-print("Datos de la 9a:")
-print(df_v1_9a)
- 
-print(df_v1.info())
+#establecer indice a la fecha
+df_bikes1 = df_bikes1.set_index(['Fecha'])
+print(df_bikes1)
+
+
+
+#df_bikes1['Calle 9a (66-39)'].plot(kind='line')
+#plt.suptitle="Calle 9a"
+#plt.show()
+
+df_bikes1_9a = df_bikes1[['Calle 9a (66-39)']].copy()#
+print(df_bikes1_9a)
+
+#print(df_bikes1.index)
+print(df_bikes1_9a.index)
+
+#df_bikes1_9a.loc[:'DiaSemana'] = df_bikes1_9a.index.weekday
+#df_bikes1_9a['DiaSemana'] = df_bikes1_9a['Fecha'].dt.day_name()
+
+df_bikes_9a_cnt = df_bikes1_9a.groupby(['Fecha']).aggregate(sum)
+#print(df_bikes1_9a['Fecha'])
+print(df_bikes_9a_cnt)
